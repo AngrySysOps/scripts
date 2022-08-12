6a7bf2cd8f2ffe2a38d2cb863580f6db52152669
Angry Admin <95914973+AngrySysOps@users.noreply.github.com>
2022-08-12 15:12:25 +0100

    Update snapshotmaxSnapshots.ps1

--- snapshotmaxSnapshots.ps1
+++ snapshotmaxSnapshots.ps1
@@ -1,7 +1,16 @@
+#This script changes limit of snapshots to 1
+#This script fix the error "Exceeded the maximum number of permitted snapshots"
+#Author: Piotr Tarnawski aka Angry Admin 
+# www.angrysysops.com
+#One MUST read README before using this script
+#Full explanationn of this code : https://angrysysops.com/2022/08/12/a-general-system-error-occurred-exceeded-the-maximum-number-of-permitted-snapshots-error-when-creating-a-virtual-machine-snapshot/
+
 Disconnect-server *
 Connect-VIServer 
 
 $snapcheck = Get-VM | where name -Like "*nsxt*"
+
+# where name -Like "*nsxt*" this is just to sort all VM with nsxt in the name, you can drop this condition. 
 
 foreach ($snap in $snapcheck) {
      $test = $snapcheck | Get-AdvancedSetting 'snapshot.maxSnapshots'
